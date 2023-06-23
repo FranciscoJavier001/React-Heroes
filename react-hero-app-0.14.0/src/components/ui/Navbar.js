199e509ec73f189d91047b9acae6575ffb99c391
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-23 05:00:48 -0600

    Change A lot of Things

--- react-hero-app-0.14.0/src/components/ui/Navbar.js
+++ react-hero-app-0.14.0/src/components/ui/Navbar.js
@@ -1,24 +1,34 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+//** Esto es un Navbar - Lo copie del Git */
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-export const Navbar = () => {
+
+//  export const Navbar = ({ history }) => { //** Aqui en pego el history, no compartir mucha informacion por los props */
+export const Navbar = () => { //** Es la linea 8, pero remplazo la sintaxis */
+
+
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        //** navbar=Para que todo este junto y no haya espacios */
+        //** navbar-expand-sm=para poner el navbar en fila y separados */
+        //** navbar-dark=letras en color gris */
+        //** bg-dark=fondo en negro */
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             
             <Link 
-                className="navbar-brand" 
-                to="/"
+                className="navbar-brand" //** Es para que salga al inicio y mas grande */
+                to="/" //** Donde me lleva cuando hago click */
             >
-                Asociaciones
+                HeroesApp {/* Texto */}
             </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+            <div className="navbar-collapse col-1"> {/* Hacer mas chico la anchura */}
+                <div className="navbar-nav">{/* Para que todas esten en hileras */}
 
                     <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
+                        activeClassName="active" //** Dond este seleccionado se quede en color blanco */
+                        className="nav-item nav-link" //** nav-item=Item de navegacion - nav-link= es para que todos se vean a la altura */
+                        exact //** Es la direccion a la que lleva */
                         to="/marvel"
                     >
                         Marvel
@@ -32,10 +42,10 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
-                    
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
+
+                    <NavLink //** Ruta que me lleva al Search en el NavBar */
+                        activeClassName="active" //** Para que se quede blanco cuando este ahi */
+                        className="nav-item nav-link"
                         exact
                         to="/search"
                     >
@@ -43,8 +53,8 @@ export const Navbar = () => {
                     </NavLink>
                 </div>
             </div>
-                
-            
+
+          
         </nav>
     )
 }
